a9f8d80c6696e44a68cac2a02b4eb5d801b2d961
kirankhatrihehe <30077446@cc.peralta.edu>
2025-12-02 23:17:12 -0800

    transaction.h

---
+++ transaction.h
@@ -0,0 +1,23 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#include <string>
+#include <ctime>
+//money record//
+class Transaction {
+private:
+    std::string type;   // "Deposit" or "Withdrawal"
+    double amount;
+    std::string timestamp;
+
+public:
+    // Constructor
+    Transaction(const std::string& type, double amount);
+
+    // Getters
+    std::string getType() const;
+    double getAmount() const;
+    std::string getTimestamp() const;
+};
+
+#endif
